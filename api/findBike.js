084ca7559f4bbdcd13ec3f4f891c819a617db6b0
Deyguerman Guzman <deyguerman@gmail.com>
2022-07-14 00:15:59 -0400

    WIP findBike - Done Funcionality - Local Dataset

--- api/findBike.js
+++ api/findBike.js
@@ -1,4 +1,42 @@
-export default function handler(req, res) {
-    const { name = 'World' } = req.query;
-    return res.send(`Hello Dey ${name}!`);
+// Data
+const bikeStations = require('../data/bikeStations.json')
+
+// Functions
+function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function getDistanceBetweenPoints(lat1, lng1, lat2, lng2, unit) {
+    // El radio del planeta tierra en metros.
+    let R = 6378137;
+    let dLat = degreesToRadians(lat2 - lat1);
+    let dLong = degreesToRadians(lng2 - lng1);
+    let a = Math.sin(dLat / 2)
+        *
+        Math.sin(dLat / 2)
+        +
+        Math.cos(degreesToRadians(lat1))
+        *
+        Math.cos(degreesToRadians(lat1))
+        *
+        Math.sin(dLong / 2)
+        *
+        Math.sin(dLong / 2);
+
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let distance = R * c;
+
+    switch (unit.toUpperCase()) {
+        case 'K':
+            return distance * 0.001
+        default:
+            return distance
+    }
+}
+
+export default async function handler(req, res) {
+    const { latitude, longitude, distance, unit = 'm' } = req.query;
+
+    const stations = bikeStations.filter(item => getDistanceBetweenPoints(latitude, longitude, item.latitude, item.longitude, unit) <= distance && item.status === 'IN_SERVICE')
+    return res.status(200).json(stations);
 }
